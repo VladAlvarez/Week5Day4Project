f667d8d4923c84d1153de968cdf0b4de48e82c24
Vlad Alvarez <moroni.v.alvarez@gmail.com>
2023-10-17 14:17:01 -0600

    first commit

---
+++ app.js
@@ -0,0 +1,7 @@
+$(document).ready(function() {
+    $('.navItem').css("color", "white");
+    $('#nav').css("background-color", "#145");
+    $('ul li:nth-child(odd)').css("background-color", "#ccc")    
+    $('ul li:nth-child(even)').css("background-color", "#aaa")    
+    $('#myInput').attr('placeholder', 'Moroni');
+  });
